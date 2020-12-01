3006f28bcddab2f4c751d7d33a41f99f00703c94
Chris Corbett <ccorbett@wayfair.com>
2020-12-01 09:51:35 -0500

    findAddsTo2020 done

--- dec01/prog01/main.go
+++ dec01/prog01/main.go
@@ -10,9 +10,12 @@ func findAddsTo2020(list []int) (int, int) {
 		fmt.Printf("range: index: %d, value: %d\n", i1, n1)
 		for _, n2 := range list[i1+1:] {
 			fmt.Printf("\tsum of %d and %d is %d\n", n1, n2, n1+n2)
+			if(n1+n2 == 2020){
+				return n1, n2
+			}
 		}
 	}
-	return 1, 2
+	return 0, 0
 }
 
 func getSecretKey(int1 int, int2 int) int {
@@ -20,7 +23,7 @@ func getSecretKey(int1 int, int2 int) int {
 }
 
 func main() {
-	fakeList := []int{2019, 1, 50}
+	fakeList := []int{2019, 50, 40, 30, 20, 10, 1}
 	int1, int2 := findAddsTo2020(fakeList)
 	secretKey := getSecretKey(int1, int2)
 	fmt.Printf("Integer #1: %d\nInteger #2: %d\n",
