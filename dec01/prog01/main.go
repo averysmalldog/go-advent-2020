2e09debda742443d553d6b0f260129531c3da93e
Chris Corbett <ccorbett@wayfair.com>
2020-12-01 09:40:35 -0500

    add palceholder secret finder function

--- dec01/prog01/main.go
+++ dec01/prog01/main.go
@@ -2,14 +2,20 @@ package main
 
 import "fmt"
 
-func findAddsTo2020(list []int) (int, int){
+func findAddsTo2020(list []int) (int, int) {
 	return 1, 2
 }
 
+func getSecretKey(int1 int, int2 int) int {
+	return 3
+}
+
 func main() {
-	fakeList := []int{1, 2}
+	fakeList := []int{2019, 1, 50}
 	int1, int2 := findAddsTo2020(fakeList)
-	fmt.Printf("Integer #1: %d.\nInteger #2: %d.\n",
+	secretKey := getSecretKey(int1, int2)
+	fmt.Printf("Integer #1: %d\nInteger #2: %d\n",
 		int1, 
 		int2)
+	fmt.Printf("Secret Key: %d\n", secretKey)
 }
