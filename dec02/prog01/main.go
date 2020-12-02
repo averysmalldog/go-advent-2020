64d39eeabdc870f94625288dacbfdf7486567b5b
Chris Corbett <ccorbett@wayfair.com>
2020-12-02 09:44:14 -0500

    done!

--- dec02/prog01/main.go
+++ dec02/prog01/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -63,25 +64,45 @@ func main() {
 	}
 
 	// solution
+	validCounter := 0
+
 	for k, v := range list {
-		fmt.Printf("Entry on line %d: %s. Contains z? ", k, v)
-		// Parse string
-		// minOccurrences
-		// maxOccurrences
+		fmt.Printf("Working on line %d. \n", k)
+		// Parse string into fields
+		str := strings.Fields(v)
+
+		// minOccurrences and maxOccurrences
+		minMax := strings.Split(str[0],"-")
+		minOccurrences, _ := strconv.Atoi(minMax[0])
+		maxOccurrences, _ := strconv.Atoi(minMax[1])
+		fmt.Printf("\tminOccurrences: %d\n", minOccurrences)
+		fmt.Printf("\tmaxOccurrences: %d\n", maxOccurrences)
+
 		// desiredLetter
+		desiredLetter := string(str[1][0])
+		fmt.Printf("\tdesiredLetter: %s\n", desiredLetter)
+
 		// password
+		password := str[2]
+		fmt.Printf("\tPassword: %s\n", password)
 
 		// Match for desired letter
-		containsA := strings.Contains(v, "z")
-		fmt.Printf("%t. ", containsA)
+		containsA := strings.Contains(v, desiredLetter)
+		fmt.Printf("\tContains %s? %t.\n", desiredLetter, containsA)
 
 		// get count if match
 		if containsA {
-			numInstances := strings.Count(v, "z")
-			fmt.Printf("Number of occurrences: %d", numInstances)
+			numInstances := strings.Count(v, desiredLetter)
+			fmt.Printf("\t\tNumber of occurrences: %d\n", numInstances)
+			if (numInstances >= minOccurrences) && (numInstances <= maxOccurrences) {
+				validCounter++
+				fmt.Println("\t\tvalid!")
+			} else {
+				fmt.Println("\t\tnot valid.")
+			}
 		}
-
-		fmt.Printf("\n")
 	}
+
+	fmt.Printf("Valid passwords: %d\n", validCounter)
 	
 }
