b91c21c5a7ba5cd1c5c90bf2ee2a2dc4961075db
Chris Corbett <ccorbett@chriss-macbook.lan>
2020-12-06 11:38:38 -0500

    day6 problem1 done!

---
+++ dec06/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+func main() {
+	// test for presence of input.txt
+	_, err := os.Stat("input.txt")
+	if os.IsNotExist(err) {
+		fmt.Println("input.txt not found. Exiting program.")
+		return
+	}
+
+	// read entire file as bytes
+	path := "input.txt"
+	dat, err := ioutil.ReadFile(path)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// solution
+	separator := "\n\n"
+	totalYeses := 0
+	entries := strings.Split(string(dat), separator)
+	for k, v := range entries {
+		answerMap := map[rune]int{}
+		answerSum := 0
+		fields := strings.Fields(v)
+		for k1, v1 := range fields {
+			for _, v2 := range v1 {
+				answerMap[v2] = 1
+			}
+			fmt.Printf("Group %d Entry %d: %s\n", k, k1, v1)
+		}
+		for _, i := range answerMap {
+			answerSum += i
+		}
+		fmt.Printf("Group %d total YESes: %d\n\n", k, answerSum)
+		totalYeses += answerSum
+	}
+	fmt.Printf("Sum of all answers: %d\n", totalYeses)
+
+}
