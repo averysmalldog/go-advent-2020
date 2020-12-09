5f0cabf6e455af6c6bb1e03a6614c5d6d218e7d8
Chris Corbett <ccorbett@wayfair.com>
2020-12-09 09:35:41 -0500

    still f'ing with it

--- dec08/prog02/main.go
+++ dec08/prog02/main.go
@@ -48,45 +48,49 @@ func main() {
 	// solution
 	for k, v := range list {
 		if v[0:3] == "nop"{
-			// fmt.Printf("Changing instruction %d from nop to jmp\n", k)
-			experiementalProgram := list
+			//fmt.Printf("Test change on %d: ", k)
+			var experiementalProgram []string
+			_ = copy(experiementalProgram, list)
 			// fmt.Printf("\t%s\n", experiementalProgram[k])
 			experiementalProgram[k] = strings.ReplaceAll(experiementalProgram[k],"nop","jmp")
 			// fmt.Printf("\t%s\n", experiementalProgram[k])
 			accumulator := 0
+			stepCounter := 1
 			programCounter := 0
 			instructionMemory := map[int]int{}
-			badInstruction := compute(experiementalProgram, &instructionMemory, &programCounter, &accumulator)
-			fmt.Printf("InstructionCache: %+v\n", instructionMemory)
+			badInstruction := compute(experiementalProgram, &instructionMemory, &programCounter, &accumulator, &stepCounter)
+			//fmt.Printf("InstructionCache: %+v\n", instructionMemory)
 			if badInstruction == -1 {
-				fmt.Printf(" YES - Program terminated! Instruction %d changed from nop to jmp, PC: %d, Accumulator: %d\n", k, programCounter, accumulator)
+				fmt.Printf("Success! Instruction %d changed from nop to jmp, PC: %d, Accumulator: %d, Steps taken: %d\n", k, programCounter, accumulator, stepCounter)
+				// fmt.Printf("InstructionCache: %+v\n", instructionMemory)
+			} else {
+				//fmt.Printf("Failure. Loops back to instruction %d.\n", badInstruction)
 			}
 		} else if v[0:3] == "jmp" {
+			//fmt.Printf("Test change on %d: ", k)
 			// fmt.Printf("Changing instruction %d from jmp to nop\n", k)
 			experiementalProgram := list
 			// fmt.Printf("\t%s\n", experiementalProgram[k])
 			experiementalProgram[k] = strings.ReplaceAll(experiementalProgram[k],"jmp","nop")
 			// fmt.Printf("\t%s\n", experiementalProgram[k])
 			accumulator := 0
+			stepCounter := 1
 			programCounter := 0
 			instructionMemory := map[int]int{}
-			badInstruction := compute(experiementalProgram, &instructionMemory, &programCounter, &accumulator)
-			//fmt.Printf("InstructionCache: %+v\n", instructionMemory)
+			badInstruction := compute(experiementalProgram, &instructionMemory, &programCounter, &accumulator, &stepCounter)
+			// fmt.Printf("InstructionCache: %+v\n", instructionMemory)
 			if badInstruction == -1 {
-				fmt.Printf(" YES - Program terminated! Instruction %d changed from jmp to nop, PC: %d, Accumulator: %d\n", k, programCounter, accumulator)
+				fmt.Printf("Success! Instruction %d changed from jmp to nop, PC: %d, Accumulator: %d, Steps taken: %d\n", k, programCounter, accumulator, stepCounter)
+				// fmt.Printf("InstructionCache: %+v\n", instructionMemory)
 			} else {
-				fmt.Printf(" badInstruction: %d", badInstruction)
+				//fmt.Printf("Failure. Loops back to instruction %d.\n", badInstruction)
 			}
-		}
+		} 
 	}
 }
 
-func compute(program []string, instructionMemory *map[int]int, programCounter *int, accumulator *int) int {
-	// if we've reached the end of the program, exit
-	if *programCounter == len(program){
-		return -1
-	}
-	
+func compute(program []string, instructionMemory *map[int]int, programCounter *int, accumulator *int, stepCounter *int) int {
+
 	// if we've been here before, this is the loop
 	localCache := *instructionMemory
 	if localCache[*programCounter] != 0 {
@@ -95,8 +99,14 @@ func compute(program []string, instructionMemory *map[int]int, programCounter *i
 
 	// vars
 	badInstruction := 0
-	localCache[*programCounter]++
+	localCache[*programCounter] = *stepCounter
 	*instructionMemory = localCache
+	*stepCounter++
+
+	// if we've reached the end of the program, exit
+	if *programCounter == len(program)-1{
+		return -1
+	}
 
 	// form up data
 	instruction := strings.Fields(program[*programCounter])
@@ -106,42 +116,36 @@ func compute(program []string, instructionMemory *map[int]int, programCounter *i
 		// fmt.Printf("PC: %d, Instruction: %s, Accumulator: %d, Next op: ", *programCounter, instruction[0], *accumulator)
 		*programCounter++
 		// fmt.Println(*programCounter)
-		return compute(program, instructionMemory, programCounter, accumulator)
-	}
-
-	// case acc
-	if instruction[0] == "acc" {
+		return compute(program, instructionMemory, programCounter, accumulator, stepCounter)
+	} else if instruction[0] == "acc" {
 		// fmt.Printf("PC: %d, Instruction: %s, Accumulator: %d, Next op: ", *programCounter, instruction[0], *accumulator)
 		if string(instruction[1][0]) == string("+") {
 			value, _ := strconv.Atoi(string(instruction[1][1:]))
 			*accumulator += value
 			*programCounter++
 			// fmt.Println(*programCounter)
-			return compute(program, instructionMemory, programCounter, accumulator)
+			return compute(program, instructionMemory, programCounter, accumulator, stepCounter)
 		}
 		if string(instruction[1][0]) == string("-") {
 			value, _ := strconv.Atoi(string(instruction[1][1:]))
 			*accumulator -= value
 			*programCounter++
 			// fmt.Println(*programCounter)
-			return compute(program, instructionMemory, programCounter, accumulator)
+			return compute(program, instructionMemory, programCounter, accumulator, stepCounter)
 		}
-	}
-
-	// case jmp
-	if instruction[0] == "jmp" {
+	} else if instruction[0] == "jmp" {
 		// fmt.Printf("PC: %d, Instruction: %s, Accumulator: %d, Next op: ", *programCounter, instruction[0], *accumulator)
 		if string(instruction[1][0]) == string("+") {
 			value, _ := strconv.Atoi(string(instruction[1][1:]))
 			*programCounter += value
 			// fmt.Println(*programCounter)
-			return compute(program, instructionMemory, programCounter, accumulator)
+			return compute(program, instructionMemory, programCounter, accumulator, stepCounter)
 		}
 		if string(instruction[1][0]) == string("-") {
 			value, _ := strconv.Atoi(string(instruction[1][1:]))
 			*programCounter -= value
 			// fmt.Println(*programCounter)
-			return compute(program, instructionMemory, programCounter, accumulator)
+			return compute(program, instructionMemory, programCounter, accumulator, stepCounter)
 		}
 	}
 
