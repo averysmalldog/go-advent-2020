64ec7fa5a91cc9c6d09deaa22c7b68760644c8ad
Chris Corbett <ccorbett@wayfair.com>
2020-12-10 09:17:35 -0500

    dec10 prog01 solved!

--- dec10/prog01/main.go
+++ dec10/prog01/main.go
@@ -47,13 +47,12 @@ func main() {
 
 	// solution part 1
 	numList := intList(list)
-	startingJoltage := 0
 	diffby1 := 0
 	diffby3 := 0
 	sort.Ints(numList)
+	startingJoltage := 0
 	
 	for k, v := range numList {
-		fmt.Printf("i: %d, v: %d, d1: %d, d3: %d\n", k, v, diffby1, diffby3)
 		difference := v-startingJoltage
 		switch difference {
 		case 1:{
@@ -65,8 +64,9 @@ func main() {
 		default: {}
 		}
 		startingJoltage = v
+		fmt.Printf("i: %d, v: %d, d1: %d, d3: %d\n", k, v, diffby1, diffby3)
 	}
-
+	diffby3++ // last jump
 	fmt.Println(numList, startingJoltage, diffby1, diffby3, diffby1*diffby3)
 
 }
